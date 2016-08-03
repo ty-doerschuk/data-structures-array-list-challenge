e256d7502e90454730488a347d1ef1ed9e5a56d1
Ty Doerschuk <tyler.doerschuk@gmail.com>
2016-08-02 22:25:24 -0400

    add solution and rspec

--- array_list.rb
+++ array_list.rb
@@ -1,3 +1,36 @@
+# ArrayList#new(size): Instantiate a new dynamic array with initial size size.
+# ArrayList#add(element): Add element to the end of the list. Return the element added.
+# ArrayList#get(index): Retrieve an element at index. If no element exists at that index, throw an OutOfBoundsException
+# ArrayList#set(index, element): Replace an existing element at index. Return the element. If no element exists at that index, throw an OutOfBoundsException
+# ArrayList#length: Return the number of items in the list
+
 class ArrayList
+
+  attr_reader :arr
+
+  def initialize(size)
+    @arr = Array.new(size)
+  end
+
+  def add(element)
+    arr << element
+    return element
+  end
+
+  def get(index)
+    arr.fetch(index)
+  end
+
+  def set(index, element)
+    if arr.length > index
+      arr[index] = element
+    else
+      raise IndexError, 'Index is not valid'
+    end
+  end
+
+  def length
+    arr.count
+  end
 
 end
--- array_list_spec.rb
+++ array_list_spec.rb
@@ -2,4 +2,40 @@ require_relative 'array_list'
 
 describe ArrayList do
 
+  let(:arraylist) { ArrayList.new(5) }
+
+  it 'initializes with a size of 5 elements' do
+    expect(arraylist.arr.length).to eq(5)
+  end
+
+  it '#add element to the end of the list' do
+    arraylist.add('foo')
+    expect(arraylist.arr[-1]).to eq('foo')
+  end
+
+  it '#add returns the element added' do
+    add = arraylist.add('foo')
+    expect(add).to eq('foo')
+  end
+
+  it '#get retrieves an element at index' do
+    arraylist.add('foo')
+    get = arraylist.get(-1)
+    expect(get).to eq('foo')
+  end
+
+  it '#set replaces an existing element at index' do
+    arraylist.set(1, 'foobar')
+    expect(arraylist.arr[1]).to eq('foobar')
+  end
+
+  it '#set returns the element' do
+    set = arraylist.set(1, 'foobar')
+    expect(set).to eq('foobar')
+  end
+
+  it '#length returns the number of items in the list' do
+    expect(arraylist.arr.length).to eq(5)
+  end
+
 end
